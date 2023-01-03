0d24df2f11dcb53ecfd697e95f49cb9acfe5d017
Carter S <101661212+ScadCarter@users.noreply.github.com>
2023-01-02 17:18:58 -0800

    can now pass arguments to change the suze of the board

--- src/main.c
+++ src/main.c
@@ -2,9 +2,20 @@
 #include "includes/game.h"
 #include "includes/print.h"
 #include "includes/screen.h"
+#include <unistd.h>
 
-int main(void) {
-  Board *board = board_init(8, 15, 0);
+int main(int argc, char **argv) {
+  int width = 8;
+  int height = 10;
+
+  if (argc == 2) {
+    width = atoi(argv[1]);
+  } else if (argc == 3) {
+    width = atoi(argv[1]);
+    height = atoi(argv[2]);
+  }
+
+  Board *board = board_init(width, height, 0);
   Screen *screen = screen_init();
 
   return game_start_interactive(&screen, &board);
